004879a9f9be41c0e373da062885401c9b19a59b
Michael Khidekel <khidmike@users.noreply.github.com>
2018-03-13 17:31:05 -0400

    dice completed

--- Python/dice.py
+++ Python/dice.py
@@ -7,6 +7,7 @@ def main():
     win = GraphWin("Dice",800,300)
     win.setCoords(0.0, 0.0, 16.0, 6.0)
     rect = Rectangle(Point(1.0,2.0), Point(3.0, 4.0))
+    rect.draw(win)
     shift = 0.0
 
 # Draw remaining squares
@@ -17,9 +18,15 @@ def main():
         shift = shift + 3.0
         new.move(shift,0)
 
-    rect.draw(win)
+# Draw markings on dice
 
-# Close out when clickedS    
+    one = Point(2.0, 3.0).draw(win)
+    two1, two2 = Point(4.5, 3.5).draw(win), Point(5.5, 2.5).draw(win)
+    three1, three2, three3 = Point(7.5, 3.5).draw(win), Point(8.0, 3.0).draw(win), Point(8.5, 2.5).draw(win)
+    four1, four2, four3, four4 = Point(10.5, 3.5).draw(win), Point(10.5, 2.5).draw(win), Point(11.5, 3.5).draw(win), Point(11.5, 2.5).draw(win)
+    five1, five2, five3, five4, five5 = Point(13.5, 3.5).draw(win), Point(14.5, 3.5).draw(win), Point(14.0, 3.0).draw(win), Point(13.5, 2.5).draw(win), Point(14.5, 2.5).draw(win)
+
+# Close out when clickedS
     win.getMouse()
     win.close()
 
